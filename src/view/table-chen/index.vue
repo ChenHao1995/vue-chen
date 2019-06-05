8c108cf57b913965b1fa0065682725f05ffc1987
陈浩 <chenhao@chenhaodeMacBook-Pro.local>
2019-06-05 19:56:28 +0800

    element-ui

--- src/view/table-chen/index.vue
+++ src/view/table-chen/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>这是tableChen</div>
+  <!-- <div>这是tableChen</div> -->
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name: "TableCChen"
+  name: "TableChen"
 };
 </script>
